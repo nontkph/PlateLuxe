f5a09a83a6a799e171319cda518f38d033bdb8e0
MIU <101027146+nontkph@users.noreply.github.com>
2024-11-09 02:08:45 +0700

    Update Default.lua

--- Baseplate/Default.lua
+++ Baseplate/Default.lua
@@ -7,23 +7,20 @@ return {
 	["Baseplate"] = function(B: BasePart)
 		local Baseplate = B or Instance.new("Part")
 		Baseplate.Name = "Baseplate"
-		Baseplate:ClearAllChildren()
 		Baseplate.TopSurface = Enum.SurfaceType.Smooth
 		Baseplate.Color = Color3.new(0.36, 0.36, 0.36)
 		Baseplate.Anchored = true
 		Baseplate.Locked = true
-		Baseplate.Size = Vector3.new(2048.00, 16.00, 2048.00)
+		Baseplate.Size = Vector3.new(2048, 16, 2048)
 		Baseplate.BottomSurface = Enum.SurfaceType.Smooth
-		Baseplate.Position = Vector3.new(0.00, -8.00, 0.00)
-		Baseplate.CFrame = CFrame.new(0.00, -8.00, 0.00, -0.00, -0.00, -1.00)
 		Baseplate.Parent = game:GetService("Workspace")
 
 		local Texture = Instance.new("Texture")
 		Texture.Face = Enum.NormalId.Top
 		Texture.StudsPerTileU = 8
-		Texture.Transparency = 0.800000011920929
+		Texture.Transparency = 0.8
 		Texture.StudsPerTileV = 8
-		Texture.Color3 = Color3.new(0.00, 0.00, 0.00)
+		Texture.Color3 = Color3.new(0, 0, 0)
 		Texture.Texture = "rbxassetid://6372755229"
 		Texture.Parent = Baseplate
 		
